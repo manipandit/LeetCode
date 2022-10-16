dddad49586ae2afe8ad62d0dc8e5390b20651e20
Manikanta Pandit <110151059+manipandit@users.noreply.github.com>
2022-10-16 11:40:15 +0530

    Added solution - LeetHub

---
+++ Equilibrium index of an array - GFG/equilibrium-index-of-an-array.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <iostream>
+using namespace std;
+#include <stdio.h>
+ 
+int findEquilibrium(int [], int );
+ 
+int main() {
+	int T;
+	cin>>T;
+	while(T--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i=0;i<n;i++)
+		cin>>a[i];
+		cout<<findEquilibrium (a,n)<<endl;
+	}
+	// your code goes here
+	return 0;
+}
+// } Driver Code Ends
+
+
+/* You are required to complete this method*/
+int findEquilibrium(int A[], int n)
+{
+  //Your code here
+  
+  int leftSum=0,totalSum=0;
+  
+  for(int i=0;i<n;i++)
+  {
+      totalSum += A[i];
+  }
+  
+  for(int i=0;i<n;i++)
+  {
+      leftSum += A[i];
+      
+      if(leftSum == totalSum)
+      {
+          return i;
+      }
+      else
+      {
+          totalSum -= A[i];
+      }
+  }
+  
+  return -1;
+}
