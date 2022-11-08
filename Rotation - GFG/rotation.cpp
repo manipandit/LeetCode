94fe15da9d754f1ab55037e0f62a2f50a498fb97
Manikanta Pandit <110151059+manipandit@users.noreply.github.com>
2022-11-08 10:00:14 +0530

    Added solution - LeetHub

--- Rotation - GFG/rotation.cpp
+++ Rotation - GFG/rotation.cpp
@@ -11,52 +11,52 @@ public:
 	int findKRotation(int arr[], int n) {
 	    // code here
 
-	   // int start=0;
-	   // int end=n-1;
+	    int start=0;
+	    int end=n-1;
 	    
-	   // while(start<=end)
-	   // {
-	   //     int mid = start + (end-start)/2;
+	    while(start<=end)
+	    {
+	        int mid = start + (end-start)/2;
 	        
-	   //     int prev = (mid - 1 + n) % n;
-    //         int next = (mid + 1) % n;
+	        int prev = (mid - 1 + n) % n;
+            int next = (mid + 1) % n;
 	        
-	   //     if(arr[mid]<=arr[prev] && arr[mid]<=arr[next])
-	   //     {
-	   //         return mid;
-	   //     }
+	        if(arr[mid]<=arr[prev] && arr[mid]<=arr[next])
+	        {
+	            return mid;
+	        }
+	        else if(arr[mid]<=arr[end])
+	        {
+	            end=mid-1;
+	        }
+	        else if(arr[mid]>=arr[start])
+	        {
+	            start=mid+1;
+	        }
 	        
-	   //     else if(arr[mid]>=arr[start])
-	   //     {
-	   //         start=mid+1;
-	   //     }
 	        
-	   //     else if(arr[mid]<=arr[end])
-	   //     {
-	   //         end=mid-1;
-	   //     }
-	   // }
-	   // return 0;
+	    }
+	    return 0;
 
-	int start = 0, end = n - 1;
-    while (start <= end) {
+// 	int start = 0, end = n - 1;
+//     while (start <= end) {
  
-        // if first element is mid or
-        // last element is mid
-        // then simply use modulo so it
-        // never goes out of bound.
-        int mid = start + (end - start) / 2;
-        int prev = (mid - 1 + n) % n;
-        int next = (mid + 1) % n;
+//         // if first element is mid or
+//         // last element is mid
+//         // then simply use modulo so it
+//         // never goes out of bound.
+//         int mid = start + (end - start) / 2;
+//         int prev = (mid - 1 + n) % n;
+//         int next = (mid + 1) % n;
  
-        if (arr[mid] <= arr[prev] && arr[mid] <= arr[next])
-            return mid;
-        else if (arr[mid] <= arr[end])
-            end = mid - 1;
-        else if (arr[mid] >= arr[start])
-            start = mid + 1;
-    }
-    return 0;
+//         if (arr[mid] <= arr[prev] && arr[mid] <= arr[next])
+//             return mid;
+//         else if (arr[mid] <= arr[end])
+//             end = mid - 1;
+//         else if (arr[mid] >= arr[start])
+//             start = mid + 1;
+//     }
+//     return 0;
 	}
 
 };
