5a7dc5c8a93f21ea3a8a8471d87113da02412db4
Manikanta Pandit <110151059+manipandit@users.noreply.github.com>
2022-11-08 09:59:38 +0530

    Added solution - LeetHub

--- Rotation - GFG/rotation.cpp
+++ Rotation - GFG/rotation.cpp
@@ -11,22 +11,22 @@ public:
 	int findKRotation(int arr[], int n) {
 	    // code here
 
-	   // int start=0,end=n-1;
-	   // int ans=0;
+	   // int start=0;
+	   // int end=n-1;
+	    
 	   // while(start<=end)
 	   // {
-	   //     int mid = (start + (end-start)/2);
+	   //     int mid = start + (end-start)/2;
 	        
-	   //     int prev = (mid-1+n)%n;
-	   //     int next = (mid+1)%n;
+	   //     int prev = (mid - 1 + n) % n;
+    //         int next = (mid + 1) % n;
 	        
 	   //     if(arr[mid]<=arr[prev] && arr[mid]<=arr[next])
 	   //     {
-	   //         ans=mid;
-	   //         break;
+	   //         return mid;
 	   //     }
 	        
-	   //     else if(arr[start]<=arr[mid])
+	   //     else if(arr[mid]>=arr[start])
 	   //     {
 	   //         start=mid+1;
 	   //     }
@@ -36,28 +36,25 @@ public:
 	   //         end=mid-1;
 	   //     }
 	   // }
-	    
-	   // return ans;
-	   int low = 0, high = n - 1;
-    //   if(arr[low]<=arr[high])
-    //   return 0;
-      /*returns 0 if array is already sorted*/
-    while (low <= high) {
+	   // return 0;
+
+	int start = 0, end = n - 1;
+    while (start <= end) {
  
         // if first element is mid or
         // last element is mid
         // then simply use modulo so it
         // never goes out of bound.
-        int mid = low + (high - low) / 2;
+        int mid = start + (end - start) / 2;
         int prev = (mid - 1 + n) % n;
         int next = (mid + 1) % n;
  
         if (arr[mid] <= arr[prev] && arr[mid] <= arr[next])
             return mid;
-        else if (arr[mid] <= arr[high])
-            high = mid - 1;
-        else if (arr[mid] >= arr[low])
-            low = mid + 1;
+        else if (arr[mid] <= arr[end])
+            end = mid - 1;
+        else if (arr[mid] >= arr[start])
+            start = mid + 1;
     }
     return 0;
 	}
